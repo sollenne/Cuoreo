cfb22e9ebaf18835c01f63b6affa265be3f5ddb3
Prasanth <pbuchi1@moneygram.com>
2018-02-20 23:39:49 -0600

    Adding spring boot app.

---
+++ src/main/java/com/mover/mover/MoverApplication.java
@@ -0,0 +1,12 @@
+package com.mover.mover;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MoverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MoverApplication.class, args);
+	}
+}
